0d6a98eb2e9e04a8d51dfbf7f063ea1c9ff47aa1
Jorge Verachtert <minimac@Mac-mini-de-Jorge.local>
2022-01-03 20:55:24 +0100

    [+] footer mobile & userController

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,13 @@
 // global js
+
+const list = document.querySelectorAll('.nav-bottom-list');
+
+function activeLink(){
+    list.forEach((item) =>
+    item.classList.remove('nav-active'));
+    this.classList.add('nav-active');
+    }
+list.forEach((item) => 
+item.addEventListener('click',activeLink));
+
+
