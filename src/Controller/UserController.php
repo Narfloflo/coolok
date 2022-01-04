5797b77c80de5f1f5ad5ed797ec6c06491d73347
Jorge Verachtert <minimac@Mac-mini-de-Jorge.local>
2022-01-04 09:53:13 +0100

    [+] rename route

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -6,6 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+#[Route('', name:'user_')]
 class UserController extends AbstractController
 {
     #[Route('/profil', name: 'profil')]
