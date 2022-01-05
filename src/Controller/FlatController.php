3939e612918551de23ecb0f3b2853c1ee8862c08
Narfloflo <92632498+Narfloflo@users.noreply.github.com>
2022-01-05 14:21:30 +0100

    [+]affichage detail un logement

--- src/Controller/FlatController.php
+++ src/Controller/FlatController.php
@@ -6,12 +6,21 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+#[Route('', name:'flat_')]
 class FlatController extends AbstractController
 {
     #[Route('/flat', name: 'flat')]
     public function index(): Response
     {
         return $this->render('flat/index.html.twig', [
+            'controller_name' => 'FlatController',
+        ]);
+    }
+
+    #[Route('/show', name: 'show')]
+    public function show(): Response
+    {
+        return $this->render('flat/show.html.twig', [
             'controller_name' => 'FlatController',
         ]);
     }
