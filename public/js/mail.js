bd1c6914497ef0bc46e7f1043bcf7e356d7374a8
NicoChlebo <nico.chlebo@gmail.com>
2022-01-12 11:02:10 +0100

    [+,~]mail on click

---
+++ public/js/mail.js
@@ -0,0 +1,6 @@
+const seeUserMail=document.getElementById('see-user-mail');
+const userMail=document.getElementById('user-mail');
+console.log('userMail');
+
+seeUserMail.addEventListener('click', function(event){userMail.classList.remove('hidden')}
+)
